c487ba7b22763571721b0ae5555913fb582db327
DennySPB <dennyspb@gmail.com>
2019-05-24 11:21:26 +0300

    Revert "stagefright: add changes related to high-framerates in CameraSource"
    This reverts commit 958db23243eed33cbc0fab45c40c91b75c4ef1b4.

--- media/libmediaplayerservice/StagefrightRecorder.cpp
+++ media/libmediaplayerservice/StagefrightRecorder.cpp
@@ -1597,7 +1597,7 @@ status_t StagefrightRecorder::setupCameraSource(
     Size videoSize;
     videoSize.width = mVideoWidth;
     videoSize.height = mVideoHeight;
-    if (mCaptureFpsEnable && mCaptureFps != mFrameRate) {
+    if (mCaptureFpsEnable) {
         if (!(mCaptureFps > 0.)) {
             ALOGE("Invalid mCaptureFps value: %lf", mCaptureFps);
             return BAD_VALUE;
@@ -1745,7 +1745,6 @@ status_t StagefrightRecorder::setupVideoEncoder(
             preferBFrames = false;
             tsLayers = 2; // use at least two layers as resulting video will likely be sped up
         } else if (mCaptureFps > maxPlaybackFps) { // slow-mo
-            format->setInt32("high-frame-rate", 1);
             maxPlaybackFps = mCaptureFps; // assume video will be played back at full capture speed
             preferBFrames = false;
         }
@@ -1999,7 +1998,7 @@ status_t StagefrightRecorder::resume() {
             mPauseStartTimeUs = bufferStartTimeUs;
         }
         // 30 ms buffer to avoid timestamp overlap
-        mTotalPausedDurationUs += (systemTime() / 1000) - mPauseStartTimeUs - 30000;
+        mTotalPausedDurationUs += resumeStartTimeUs - mPauseStartTimeUs - 30000;
     }
     double timeOffset = -mTotalPausedDurationUs;
     if (mCaptureFpsEnable && mVideoSource != VIDEO_SOURCE_SURFACE) {
